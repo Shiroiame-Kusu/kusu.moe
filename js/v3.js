ae3935c486ab3e875f7ea79891969aad6b7785b5
楠ちゃん <amenai@bwmc.live>
2022-09-05 09:47:12 +0800

    Ver 1.0

---
+++ js/v3.js
@@ -0,0 +1,6 @@
+var windowWidth = $(window).width();
+var meting_api = 'https://music-api.bwmc.live/?type=:type&id=:id';
+console.log(`Kusu.moe Redirecting
+Based On kaygb-v3`)
+console.log("新版KZHomePage已经准备就绪，欢迎使用体验：https://github.com/kaygb/KZHomePage ")
+console.log('\n' + ' %c kaygb-v3 by kaygb ' + ' %c https://blog.170601.xyz/archives/25.html ' + '\n' + '\n', 'color: #fff; background: #fd79a8; padding:5px 0;', 'background: #FFF; padding:5px 0;');
